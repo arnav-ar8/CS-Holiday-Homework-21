c5044b6add7eac09b901fe56ca904f415a6e9d22
Arnav <57841763+arnav-ar8@users.noreply.github.com>
2021-11-13 23:23:25 +0400

    Update main-code.py

--- main-code.py
+++ main-code.py
@@ -37,12 +37,12 @@ if click.confirm(f"Do you want to start?", default=True):
 def switch(action): 
     if action == 1: 
   
-       s() 
+       s()
        print ("1. Simple Bonus finder using two if statements") 
-       s() 
+       s()
   
        sale1 = float(input("Enter Monthly Sales you fulfilled (in $): ")) 
-       s() 
+       s()
   
        commission = 0 
   
@@ -54,8 +54,20 @@ def switch(action):
           commission = sale1 * 5/100 
           print (f"You are eligible for a 5% commission! ${commission} is your commission earned!") 
   
-     
        s() 
+    
+        print("Welcome to the Voting Eligibility Checker")
+        age = int(input("Enter your age to check your eligibility to vote: "))
+
+        if age>=18:
+          print("Congratulations! You are eligible to vote!")
+
+        if age<18:
+          print(f"Sorry, you can't vote yet. Wait for {18-age} more years to be able to vote.")
+          
+       s()
+      
+      
   
 if action == 2: 
   
@@ -71,12 +83,46 @@ if action == 2:
         print (f"{eveodd} is an odd number!") 
   
     s() 
-  
+    
+    nOne = int(input("Enter the first number: "))      
+    nTwo = int(input("Enter the second number: ")) 
+    if nOne >= nTwo:   
+      if nOne == nTwo:    
+         print("Both numbers are equal.")       
+      else:  
+         print(f"The first number ({nOne}) is greater than the second number ({nTwo})")
+    else:   
+      print(f"The second number ({nTwo}) is greater than the first number ({nOne})") 
+      
+    s()
+    
+    n1111 = int(input("Enter any number to check whether it is positive or negative: "))
+
+    if n1111==0:
+        print(f"The number {n1111} is equal to 0")
+    if n1111>0:
+        print(f"The number {n1111} is a positive number")
+    else:
+        print(f"{n1111} is a negative number.")
+    
+    s()
+    
+    print("Program to check whether there are any numbers in a given sentence")
+    
+    sentence1 = input("Enter any sentence (it may include numbers): ")
+    numbers = {"1","2","3","4","5","6","7","8","9","10","0"}
+
+    ss1 = set(sentence1.split())
+    if ss1.intersection(numbers):
+      print("There are numbers in the sentece")
+    else:
+      print("There are no numbers in the sentence")
+     
+    s()
     l() 
     s() 
-  
-    print ("2. Finding the largest number out of three numbers") 
-    l() 
+if action==3:
+    print ("Finding the largest number out of three numbers") 
     Ln1 = float(input("Enter any number: ")) 
     Ln2 = float(input("Enter another number: ")) 
     Ln3 = float(input("Enter one more number: ")) 
@@ -101,23 +147,18 @@ if action == 2:
   
     else: 
         print ("hmm... I don't get it, try again please :) ") 
-  
-    print("The elif statement was also used above.") 
-  
+    
     s() 
-    l() 
-  
-if action==3: 
-    l() 
-    print ("1. Finding the type of the triangle with three sides provided") 
-    s() 
-     
+    
+    print ("Finding the type of the triangle with three sides provided") 
+    s()
+    
     s1 = float(input("Enter the length of the first side of the triangle: ")) 
-    s() 
+    s()
     s2 = float(input("Enter the length of the  second side of the triangle: ")) 
-    s() 
+    s()
     s3 = float(input("Enter the length of the third side of the triangle: ")) 
-    s() 
+    s()
   
     if s1==s2==s3: 
         print("This triangle is an Equilateral triangle since all three sides are equal!") 
@@ -131,14 +172,13 @@ if action==3:
   
     s() 
     l() 
-    print("2. Calculating the roots of the quadratic equation in the form of ax^2 + bx + c = 0") 
-  
-    s() 
+    print("Calculating the roots of the quadratic equation in the form of ax^2 + bx + c = 0") 
+    s()
+    
     Qa = float(input("Input the value of a from the equation: ")) 
     Qb = float(input("Input the value of b from the equation: ")) 
     Qc = float(input("Input the value of c from the equation: ")) 
   
-    l() 
   
     if Qa == 0: 
         print ('Hey! The value of "a" can not be zero!') 
@@ -148,71 +188,76 @@ if action==3:
   
      
   
-    if DiscR > 0: 
+    elif DiscR > 0: 
         print ("Finding roots...") 
-        l() 
+        l()
         Rt1 = ((-Qb) + math.sqrt(DiscR)) / 2*Qa 
         Rt2 = ((-Qb) - math.sqrt(DiscR)) / 2*Qa 
         print (f"There are 2 roots for this equation! They are {Rt1} and {Rt2}") 
   
-    if DiscR == 0: 
+    elif DiscR == 0: 
         print ("Finding roots...") 
-        l() 
+        l()
         Rt3 = (-Qb) / 2*Qa 
         print (f"There is 1 root for this equation! It is {Rt3}") 
   
-    if DiscR < 0: 
+    elif DiscR < 0: 
         print ("This equation has no real roots") 
+    
+    else:
+        print("Invalid inputs provided. Please enter valid values.")
   
   
     l() 
   
-    print("3. Program to identify two numbers and an arithmetic operator and display the results") 
+    print("Program to identify two numbers and an arithmetic operator and display the results") 
   
     One = int(input("Enter any number: ")) 
     Two = int(input("Enter another number: ")) 
     operator = (input("Enter any arithmetic operator (+, -, *, /): ")) 
                     
     if operator=="+": 
-        s() 
+         
         print(f"Ok, so you'll add!\nHere is the answer :) \n{One+Two}") 
   
     elif operator=="-": 
-        s() 
+        
         print(f"Ok, so you'll subtract the first number from the second one!\nHere is the answer :) \n{One-Two}") 
   
     elif operator=="*": 
-        s() 
+         
         print(f"Ok, so you'll multiply!\nHere is the answer :) \n{One*Two}") 
   
     elif operator=="/": 
-        s() 
+        
         print(f"Ok, so you'll divide the first number by the second one!\nHere is the answer :) \n{One/Two}") 
   
     else: 
-        s() 
-        s() 
         print("I didn't get that, please try again! :)") 
   
   
     l() 
   
+  
     print("4. Grade calculation program") 
-    s() 
+    s()
     print("All marks are out of 100") 
-    l() 
+    s()
+    
     S1 = int(input("Enter your marks for subject 1: ")) 
     S2 = int(input("Enter your marks for subject 2: ")) 
     S3 = int(input("Enter your marks for subject 3: ")) 
     S4 = int(input("Enter your marks for subject 4: ")) 
-    S5 = int(input("Enter your marks for subject 5: ")) 
-  
+    S5 = int(input("Enter your marks for subject 5: "))
+    
+    s()
+    l()
+    s()
+    
     S_total = S1 + S2 + S3 + S4 + S5 
     S_avg = S_total/5 
-  
          
     print (f"Your total percentage is {S_avg}%!") 
-     
   
     if S_avg >=90: 
         print("Your grade is A+!") 
@@ -238,8 +283,8 @@ if action==3:
   
     l() 
   
-    print ("5. Arranging three numbers in ascending order") 
-    l() 
+    print ("Arranging three numbers in ascending order") 
+     
     AN1 = float(input("Enter any number: ")) 
     AN2 = float(input("Enter another number: ")) 
     AN3 = float(input("Enter one more number: ")) 
@@ -269,17 +314,160 @@ if action==3:
   
 if action==4: 
     l() 
-    print("Coming Soon") 
+    print("Finding the factorial of a number using the for loop")
+    nt = int(input("enter a number: "))
+    fact = 1
+    
+    if nt==0:
+      print("The factorial of 0 is 1")
+      
+    elif nt<0:
+      print("Negative numbers do not have factorials")
+      
+    elif nt>0:
+      for i in range(1, nt + 1):
+        fact = fact * i
+      print(f"The factorial of {nt} is {fact}")
+     
+    else:
+      print("Error. Please rerun the program with a valid input.")
+      
+    s()
+    l()
+    s()
+    
+    print("Prime number checker")
+    npr = int(input("Enter any number: "))
+
+    if npr>1:
+      for i in range(2,npr):
+          if (npr%i)==0:
+              print(f"{npr} is not a prime number")
+              print(f"Because: {i} times {npr//i} is {npr}")
+              break
+          else:
+            print(f"{npr} is a prime number")
+      
+    else:
+      print(f" {npr} is not a prime number")
+      
+      s()
+    
+    print("Calculating the sum of numbers upto a given number")
+    nsr = int(input("Enter Number to calculate the sum of numbers until: "))
+
+    sm1 = 0
+    for i in range(0, nsr+1, 1):
+        sm1 += i
+    print(f"The sum of the first {nsr} numbers is {sm1}")
+    
+    s()
+    
+    print("Sum of digits in a number calculator")
+    
+    nsd = int(input("Enter any number: "))
+    sdn = 0
+    for i in str(nsd):
+      sdn += int(i)
+    print(sdn)
+    
+    s()
+    
+    print("Fibonacci sequence calculator")
+    nfib = int(input("Number of terms required: "))
+    ft = 0
+    st = 1
+    if nfib==1:
+      print(ft)
+    else:
+      print(ft)
+      print(st)
+      for i in range(2, nfib):
+        tt = ft + st
+        ft = st
+        st = tt
+        print(tt)
+        
+    s()
+    l()
+    s()
+    
+    if action==5:
+    l()
+    
+   s()
+  print("Armstrong number checker")
+  N1 = int(input ("Enter any number: "))
+  sum1 = 0
+  x = N1
+
+  temp = x
+
+  while temp>0:
+      digit = temp%10
+      sum1 = sum1 + digit**3
+      temp = temp//10
+
+  if sum1==x:
+      print(f"{N1} is an Armstrong number")
+  else:
+      print(f"{N1} is not an Armstrong number")
+      
+  s()
   
+  print("Factorial calculator using a while loop")
+  factN = int(input("Enter any positive whole number: "))
+  factorial = 1
+
+  if factN < 0:
+      print ("I told you to input a positive number, didn't you read >:(")
+      print ("Factorial doesn't work for negative numbers!!!")
+
+  elif factN == 0:
+      print ("The factorial of 0 is always 1 :)")
+
+  else:
+      while (i>0):
+          factorial = factorial * i
+          i = i + 1
+      print(f"The factorial of {factN} is {factorial}")
+      
+  s()
   
-if action==5: 
-    l() 
-    print("Coming Soon") 
+  print("Calculating the sum of numbers till a given number using the while loop")
+  nw = int(input("Enter any number to find the sum of all numbers upto it: "))
+
+  sw = 0
+  i = 1
+
+  while i<=nw:
+      sw += i
+      i = i+1 
+
+  print(f"The sum is {sw}")
+  
+  s()
+  l()
+  s()
+  
+    
   
 if action==6: 
     l() 
-    print("Coming Soon") 
-     
+    s()
+    
+    print("Triangular pattern generator")
+    ch = input("Enter any number/character to make a triangular pattern from: ")
+
+    rw = int(input(f'Enter the number of rows of "{ch}" you want: '))
+
+    for i in range(1, rw + 1):
+        for c in range(1, i + 1):
+            print(ch, end=" ")
+        print("")
+        
+        
+        
 if action==7: 
     l() 
     print("1. Menu driven program to calculate the area of a Circle, Triangle, Square and Rectangle") 
